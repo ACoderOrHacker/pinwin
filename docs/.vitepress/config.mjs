a1a09101d4a436f3aa1ca2cd4b38ef19e7491a19
ACoderOrHacker <sgy2788@163.com>
2025-07-26 21:50:13 +0800

    docs(image): add logo image

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   base: '/pinwin/',
+  head: [
+    ["link", { rel: "icon", href: `/pinwin/logo.ico` }],
+  ],
   title: "Pinwin",
   description: "A tool for pining windows to the top of your screen",
   themeConfig: {
