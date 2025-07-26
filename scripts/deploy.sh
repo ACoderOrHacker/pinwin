442153b8fe34d9f17d3710dd703336530a37cd12
ACoder <sgy2788@163.com>
2025-07-26 09:28:02 +0800

    fix(docs): fix deploy shell and ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,10 +7,13 @@ npm run docs:build
 cd docs/.vitepress/dist
 rm -rf .git
 
+git config user.name "ACoderOrHacker"
+git config user.email "sgy2788@163.com"
+
 git init
 git add -A
 git commit -m 'deploy docs'
 git branch -m master main
 
-git push -f https://github.com/ACoderOrHacker/pinwin.git main:docs
+git push -f https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/ACoderOrHacker/pinwin.git main:docs
 cd -
