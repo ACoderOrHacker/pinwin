f25bd78b96c7fbfda021ab37c731337ec502aa6d
ACoder <sgy2788@163.com>
2025-07-26 09:15:31 +0800

    docs(pinwin): add docs

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run docs:build
+
+cd docs/.vitepress/dist
+rm -rf .git
+
+git init
+git add -A
+git commit -m 'deploy docs'
+git branch -m master main
+
+git push -f https://github.com/ACoderOrHacker/pinwin.git main:docs
+cd -
